45fdac62a3c58124288292510981953fcd4b84ce
Shawn <hsiang.yu0129@gmail.com>
2025-03-19 14:34:20 +0800

    add load four different json file function

--- src/preprocessing/eye/Eye.py
+++ src/preprocessing/eye/Eye.py
@@ -1,21 +1,79 @@
 import pandas as pd
-
+import json
 class Eye:
     def __init__(self):
         
         pass
 
-    def load(self) -> pd.DataFrame:
+    def load_gaze(self) -> pd.DataFrame:
+        path = '/Users/shawn/Desktop/acrophobia/elevatordata/54273/acrophobiapico_elevator_20241108033202_EyeCombinedData.json'
+        strData = open(path, 'r', encoding='utf-8-sig').read()
+        listData = strData.split('\n')
+        listData = listData[:len(listData)-1]
+        eye_data = []
+        for data in listData:
+            temp = []
+            temp.append(json.loads(data)['Timestamp'].replace('+08:00', ''))
+            temp.append(json.loads(data)['CombineEyeGazePoint'])
+            temp.append(json.loads(data)['CombineEyeGazeVector'])
+            if temp[1]['x'] == 0.0 :
+                continue
+            if temp[2]['x'] == 0.0 :
+                continue
+            eye_data.append(temp)
+        df = []
+        df = pd.DataFrame(eye_data,columns=['Timestamp', 'CombineEyeGazePoint', 'CombineEyeGazeVector'])
+        return df
+    
+    def load_position(self) -> pd.DataFrame:
+        path = '/Users/shawn/Desktop/acrophobia/elevatordata/54273/acrophobiapico_elevator_20241108033202_EyeLeftRightData.json'
+        strData = open(path, 'r', encoding='utf-8-sig').read()
+        listData = strData.split('\n')
+        listData = listData[:len(listData)-1]
+        eye_data = []
+        for data in listData:
+            temp = []
+            temp.append(json.loads(data)['Timestamp'].replace('+08:00', ''))
+            temp.append(json.loads(data)['LeftEyePositionGuide'])
+            temp.append(json.loads(data)['LeftEyeOpenness'])
+            temp.append(json.loads(data)['RightEyePositionGuide'])
+            temp.append(json.loads(data)['RightEyeOpenness'])
+            if temp[1]['x'] == 0.0 :
+                continue
+            if temp[3]['x'] == 0.0 :
+                continue
+            eye_data.append(temp)
+        df = []
+        df = pd.DataFrame(eye_data,columns=['Timestamp', 'LeftEyePositionGuide', 'LeftEyeOpenness','RightEyePositionGuide','RightEyeOpenness'])
+        print(df.head(50))
+        return df
         
+    def load_focus(self) -> pd.DataFrame:
+        path = '/Users/shawn/Desktop/acrophobia/elevatordata/54273/acrophobiapico_elevator_20241108033202_EyeFocusData.json'
+        strData = open(path, 'r', encoding='utf-8-sig').read()
+        listData = strData.split('\n')
+        listData = listData[:len(listData)-1]
+        eye_data = []
+        for data in listData:
+            temp = []
+            temp.append(json.loads(data)['Timestamp'].replace('+08:00', ''))
+            temp.append(json.loads(data)['FocusName'])
+            temp.append(json.loads(data)['FocusPoint'])
+            temp.append(json.loads(data)['FocusNormal'])
+            temp.append(json.loads(data)['FocusDistance'])
+            eye_data.append(temp)
+        df = []
+        df = pd.DataFrame(eye_data,columns=['Timestamp', 'FocusName', 'FocusPoint','FocusNormal','FocusDistance'])
+        print(df.head(50))
+        return df
 
+    def preprocessing(self):
         pass
 
-    def prepossessing(self):
-        pass
 
 
 
-open()
 
-
+newEye = Eye()
+newEye.load_focus()
     
